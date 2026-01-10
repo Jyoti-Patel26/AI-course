ca880b0a938ebd9f2b95d0e08c607ff84bcb2d48
Jyoti Patel <jp3167252@gmail.com>
2026-01-10 20:52:04 +0530

    Update p1.py

--- DAY-2/p1.py
+++ DAY-2/p1.py
@@ -1,1 +1,21 @@
 
+age = 10
+print(age)
+print(type(age))
+
+per=85.23
+print(per)
+print(type(per))
+
+name="Ai"
+print(name)
+print(type(name))
+
+isPresent=True
+print(isPresent)
+print(type(isPresent))
+
+class_ai=None
+print(class_ai)
+print(type(class_ai))
+
