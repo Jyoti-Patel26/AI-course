f1591141b87456a5a333beed700985df19ccf29d
Jyoti Patel <jp3167252@gmail.com>
2026-01-10 20:56:49 +0530

    Create p6.py

---
+++ DAY-2/p6.py
@@ -0,0 +1,15 @@
+
+person={
+    "firstname":"abs",
+    "middlenae":"ass",
+    "lastname":"xxy",
+    "parent":{
+        "father":{
+            "name":"aaa"
+        },
+        "mother":{
+
+        }
+    },
+    "qualification":
+}
