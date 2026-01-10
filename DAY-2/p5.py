f79240d36537a71a90ce2de65cae4577cd3927ad
Jyoti Patel <jp3167252@gmail.com>
2026-01-10 20:56:13 +0530

    Create p5.py

---
+++ DAY-2/p5.py
@@ -0,0 +1,15 @@
+
+points={1,44,77,44,33,1}
+
+print(points)
+print(type(points))
+
+cart=["pc","chocolate","colodrink","chocolate","pc","shoes"]
+print(cart)
+
+c_unique=set(cart)
+print(c_unique)
+
+c_list=list(c_unique)
+print(c_list)
+
